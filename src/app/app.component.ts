26320308e3c9de7fca48117ca1a9c905fea94c85
theGeekkit <111545346+theGeekkit@users.noreply.github.com>
2022-10-03 16:51:26 -0500

    Mutants 10-3-22

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Mutants';
+  title = 'S.H.I.E.L.D. Mutant Database';
 }
---
+++ src/app/mutant/mutant.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Mutant } from '../Model/mutant';
+
+@Component({
+  selector: 'app-mutant',
+  templateUrl: './mutant.component.html',
+  styleUrls: ['./mutant.component.css']
+})
+export class MutantComponent implements OnInit {
+
+  @Input() mutant: Mutant = {  alias: "",
+    name: "",
+    powers: "",
+    imagePath: ""}
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
