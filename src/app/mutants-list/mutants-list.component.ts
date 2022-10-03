f110cd1fc369439ba33db370c633b1827715ded7
theGeekkit <111545346+theGeekkit@users.noreply.github.com>
2022-10-03 18:18:53 -0500

    10-3-22

--- src/app/mutants-list/mutants-list.component.ts
+++ src/app/mutants-list/mutants-list.component.ts
@@ -8,12 +8,7 @@ import { Mutant } from '../Model/mutant';
 })
 export class MutantsListComponent implements OnInit {
 
-  mutants: Mutant[] = [ {
-    alias:"Sub-Mariner",
-    name: "Namor",
-    powers: "Stronger and tougher than average human or atlantean. Strength further enhanced when underwater or doused with H20. Telepathic abilities derived from atlantean heritage. Namor also has the ability to fly for indefinite periods due to small wings on his ankles. He also possesses indeterminite longevity as he has shown no signs of aging even though he is several decades old.",
-    imageUrl:'https://i.postimg.cc/j5LpyBN6/Namor.jpg'
-  },
+  mutants: Mutant[] = [
   {
     alias:"Angel",
     name: "Warren Worthington III",
@@ -21,10 +16,22 @@ export class MutantsListComponent implements OnInit {
     imageUrl:'https://thecomicvault.files.wordpress.com/2017/01/comics-marvel_00364983-970x545.jpg'
   },
   {
+    alias:"Banshee",
+    name: "Sean Cassidy",
+    powers: "",
+    imageUrl:'https://static.wikia.nocookie.net/superheroes/images/8/89/Banshee.jpg/revision/latest?cb=20171202122956'
+  },
+  {
     alias:"Beast",
     name: "Henry 'Hank' McCoy",
     powers: "Superhuman agility and athletic prowess further enhanced by serum that altered his physical appearance to be covered in blue fur and be overall more bestial",
     imageUrl:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSUCI8wWFyPMUh7oDWqHAv2_ZZ3_qqMa2zWPQ&usqp=CAU'
+  },
+  {
+    alias:"Sub-Mariner",
+    name: "Namor",
+    powers: "Stronger and tougher than average human or atlantean. Strength further enhanced when underwater or doused with H20. Telepathic abilities derived from atlantean heritage. Namor also has the ability to fly for indefinite periods due to small wings on his ankles. He also possesses indeterminite longevity as he has shown no signs of aging even though he is several decades old.",
+    imageUrl:'https://i.postimg.cc/j5LpyBN6/Namor.jpg'
   },
 ]
 
