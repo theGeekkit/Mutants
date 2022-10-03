f807e46ea366906acb67f796327d307367cc4b18
theGeekkit <111545346+theGeekkit@users.noreply.github.com>
2022-10-03 16:51:48 -0500

    Mutants 10-3-22

---
+++ src/app/Model/mutant.ts
@@ -0,0 +1,15 @@
+export class Mutant {
+  alias: string;
+  name: string;
+  powers: string;
+  imagePath: string;
+
+  constructor(alias: string, name: string, powers: string, imagePath: string) {
+    this.alias = alias
+    this.name = name
+    this.powers = powers
+    this.imagePath = imagePath
+
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatCardModule} from '@angular/material/card';
+import { MutantsListComponent } from './mutants-list/mutants-list.component';
+import { MutantComponent } from './mutant/mutant.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MutantsListComponent,
+    MutantComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatGridListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
